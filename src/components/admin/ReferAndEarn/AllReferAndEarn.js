d4a1d32861929106f34e21bb5def845e1dcb1077
Jahid Hasan <jahidhasan2856@gmail.com>
2022-05-12 16:29:51 +0600

    refers and contacts added spinner

--- src/components/admin/ReferAndEarn/AllReferAndEarn.js
+++ src/components/admin/ReferAndEarn/AllReferAndEarn.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { GridLoader } from "react-spinners";
 import { getData, getUserData } from "../../../../__lib__/helpers/HttpService";
 import ReferEarnCard from "./ReferEarnCard";
 
 const AllReferAndEarn = () => {
   const [refers, setRefers] = useState([]);
-
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#27d37f");
 
   useEffect(() => {
     getData("/refers").then((res) => setRefers(res));
@@ -14,7 +16,15 @@ const AllReferAndEarn = () => {
   return (
     <secttion>
       <div className="row">
-        {refers.length > 0 && refers?.map((refer,i) => <ReferEarnCard refer={refer} key={i}/>)}
+        {refers.length > 0 ? (
+          refers?.map((refer, i) => <ReferEarnCard refer={refer} key={i} />)
+        ) : (
+          <div className="p-4">
+            <div className="d-flex justify-content-center align-items-center">
+              <GridLoader color={color} loading={loading} size={8} />
+            </div>
+          </div>
+        )}
       </div>
     </secttion>
   );
