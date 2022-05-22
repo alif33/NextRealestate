2ddf5d2ee880ef14bee1c64aa1827e606a8fac43
Jahid Hasan <jahidhasan2856@gmail.com>
2022-05-22 11:48:07 +0600

    api some change

--- pages/api/subscriber/[id]/index.js
+++ pages/api/subscriber/[id]/index.js
@@ -1,22 +1,22 @@
-import nc from 'next-connect';
-import Product from '../../../../models/Product';
-import { isAdmin } from '../../../../utils/auth';
-import db from '../../../../utils/db';
+// import nc from 'next-connect';
+// import Product from '../../../../models/Product';
+// import { isAdmin } from '../../../../utils/auth';
+// import db from '../../../../utils/db';
 
-const handler = nc();
+// const handler = nc();
 
-handler.get(async (req, res) => {
-  await db.connect();
-  const product = await Product.findById(req.query.id);
-  await db.disconnect();
-  res.send(product);
-});
+// handler.get(async (req, res) => {
+//   await db.connect();
+//   const product = await Product.findById(req.query.id);
+//   await db.disconnect();
+//   res.send(product);
+// });
 
-handler.use(isAdmin).delete(async (req, res) => {
-  await db.connect();
-  const product = await Product.findById(req.query.id);
-  await db.disconnect();
-  res.send(product);
-});
+// handler.use(isAdmin).delete(async (req, res) => {
+//   await db.connect();
+//   const product = await Product.findById(req.query.id);
+//   await db.disconnect();
+//   res.send(product);
+// });
 
-export default handler;
+// export default handler;
