b5fa44e0e81a1c499e908398469a9635cac391c4
Jahid Hasan <jahidhasan2856@gmail.com>
2022-06-13 10:42:33 +0600

    wishlist check is already added property

--- pages/api/user/wishlist/[PID].js
+++ pages/api/user/wishlist/[PID].js
@@ -1,5 +1,6 @@
 import nc from "next-connect";
 import Property from "../../../../models/Property";
+import User from "../../../../models/User";
 import { isAuth } from "../../../../utils/auth";
 import db from "../../../../utils/db";
 
@@ -8,26 +9,40 @@ const handler = nc();
 handler.use(isAuth).put(async (req, res) => {
   const { PID } = req.query;
   await db.connect();
-  const property = await Property.find({ _id: 'dfdfdfdfasd' });
-   db.disconnect();
-  console.log(property);
-  //   if (property) {
-  //     const userUpdate = await User.updateOne(
-  //       { _id: req.user._id },
-  //       {
-  //         $push: {
-  //           wishlists: PID,
-  //         },
-  //       },
-  //       { returnOriginal: false }
-  //     );
-  //     await db.disconnect();
-  //     res.send(userUpdate);
-  //   } else {
-  //     res.send({
-  //       error: "Property not found",
-  //     });
-  //   }
+  const property = await Property.find({ _id: PID });
+  if (property.length > 0) {
+    const alreadyExists = await User.findById({ _id: req.user._id });
+
+    if (!alreadyExists.wishlists.includes(PID)) {
+      const userUpdate = await User.findByIdAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: {
+            wishlists: PID,
+          },
+        },
+        { returnOriginal: false }
+      );
+
+      await db.disconnect();
+
+      if (userUpdate) {
+        res.send(userUpdate);
+      } else {
+        res.send({
+          error: "Unauthorized",
+        });
+      }
+    } else {
+      res.send({
+        error: "Already added on wishlisht",
+      });
+    }
+  } else {
+    res.send({
+      error: "Property not found",
+    });
+  }
 });
 
 export default handler;
