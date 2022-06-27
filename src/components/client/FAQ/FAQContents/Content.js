9b3d34bbd9c09d2dc072eec23cd92e0adf871ce7
Jahid Hasan <jahidhasan2856@gmail.com>
2022-06-27 23:11:50 +0600

    faqs page owner, tenant, referral content added

--- src/components/client/FAQ/FAQContents/Content.js
+++ src/components/client/FAQ/FAQContents/Content.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { useState } from "react";
+import { ownerContents, referContents, tenantContents } from "./contentData";
+
 
 const Content = () => {
+  const [isActive, setIsActive] = useState("owner")
   return (
     <>
       <section className="container mb-md-5 mb-4 pb-lg-5">
@@ -22,17 +25,17 @@ const Content = () => {
               <div className="offcanvas-body">
                 <ul className="nav nav-tabs navbar-nav flex-column m-0 text-lg-center">
                   <li className="nav-item px-0">
-                    <a className="nav-link active px-3" href="#">
+                    <a className={`nav-link px-3 ${isActive === 'owner' ? "active": ""}`} onClick={() => setIsActive('owner')} href="#">
                       For Owners
                     </a>
                   </li>
                   <li className="nav-item px-0">
-                    <a className="nav-link px-3" href="#">
+                    <a className={`nav-link px-3 ${isActive === 'tenant' ? "active": ""}`} onClick={() => setIsActive('tenant')} href="#">
                       For Tenants
                     </a>
                   </li>
                   <li className="nav-item px-0">
-                    <a className="nav-link px-3" href="#">
+                    <a className={`nav-link px-3 ${isActive === 'referral' ? "active": ""}`} onClick={() => setIsActive('referral')} href="#">
                       For Referrals
                     </a>
                   </li>
@@ -40,70 +43,50 @@ const Content = () => {
               </div>
             </div>
           </aside>
-          {/* Content*/}
-          <div className="col-lg-9 offset-xl-1 col-lg-8">
-            <div className="pb-md-2">
-              <h3 className="h5">
-                How soon you will find tenant after I list the property?
-              </h3>
-              <p>
-                After you post your property, we will do verification of your
-                property and find suitable tenant for your property that won’t
-                take more than couple of hours or maximum 2-3 days.
-              </p>
-            </div>
-            <div className="pb-md-2">
-              <h3 className="h5">
-                Do I need to pay anything in advance to use your services?
-              </h3>
-              <p>
-                No, in order to rent a residential property, you no need to pay
-                any brokerage and you can avail our services without any advance
-                payment too, We believe in service first and pay later policy.
-              </p>
-            </div>
-            <div className="pb-md-2">
-              <h3 className="h5">
-                What&apos;s the rent that I can get for my property?
-              </h3>
-              <p>
-                Your Relationship Manager has city level knowledge of rents in
-                various localities, they will guide you about the correct rent
-                to be quoted for your property.
-              </p>
-            </div>
-            <div className="pb-md-2">
-              <h3 className="h5">
-                Will I get good tenants as per my expectation for my property?
-              </h3>
-              <p>
-                Yes, your Relationship Manager will meet tenants personally and
-                choose them according to your requirements only. We will get you
-                tenants of your choice for your property.
-              </p>
-            </div>
-            <div className="pb-md-2">
-              <h3 className="h5">
-                Will you assist in making rental agreement?
-              </h3>
-              <p>
-                Yes, we will either help you in making of hard copy of rental
-                agreement or send you a soft copy of the agreement over an
-                e-mail. You must have Pan Card, Aadhar Card and passport size
-                Photos for the agreement to be prepared.
-              </p>
-            </div>
-            <div className="pb-md-2">
-              <h3 className="h5">
-                When do I need to pay your service charges?
-              </h3>
-              <p>
-                Once we find suitable tenant for your property, you need to pay
-                our service charges before we make a rent agreement and handover
-                your house to tenant.
-              </p>
-            </div>
-          </div>
+          {/* Content For Owners*/}
+          {isActive === 'owner' && <div className={`col-lg-9 offset-xl-1 col-lg-8`}>
+            {ownerContents.map((content, i) => (
+              <div key={content.id} className="pb-md-2">
+                <h3 className="h5">
+                  {content.title}
+                </h3>
+                <p>
+                  {content.description}
+                </p>
+              </div>
+            ))}
+
+          </div>}
+
+
+          {/* Content  For Tenants*/}
+          {isActive === 'tenant' && <div className={`col-lg-9 offset-xl-1 col-lg-8`}>
+            {tenantContents.map((content, i) => (
+              <div key={content.id} className="pb-md-2">
+                <h3 className="h5">
+                  {content.title}
+                </h3>
+                <p>
+                  {content.description}
+                </p>
+              </div>
+            ))}
+          </div>}
+          {/* Content  For Referral*/}
+
+          {isActive === 'referral' && <div className={`col-lg-9 offset-xl-1 col-lg-8`}>
+            {referContents.map((content, i) => (
+              <div key={content.id} className="pb-md-2">
+                <h3 className="h5">
+                  {content.title}
+                </h3>
+                <p>
+                  {content.description}
+                </p>
+              </div>
+            ))}
+          </div>}
+
         </div>
       </section>
     </>
